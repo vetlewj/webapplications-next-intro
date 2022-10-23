90c66ccd1debd2ff5c86b3e4fafe09e6846f97ef
vetlewj <vetlewj@gmail.com>
2022-10-23 22:22:38 +0200

    List movies in ul

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -7,31 +7,38 @@ export default function Movies() {
   const getMovies = async () => {
     try {
       const response = await axios.get('api/movies')
-      setMovies(response.data)
+      setMovies(response.data.data)
     } catch (error) {
       console.log(error)
     }
   }
   useEffect(() => {
     getMovies()
-  }, [movies])
+  }, [])
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     await axios.post('api/movies/', {
-      id: 0,
+      id: movies.length + 1,
       title: e.currentTarget.movieTitle.value,
       year: e.currentTarget.year.value,
       rating: e.currentTarget.rating.value,
     })
-
     await getMovies()
   }
 
   return (
     <>
       <h1>Movies</h1>
-      <section>{JSON.stringify(movies)}</section>
+      <section>
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              {movie.title} ({movie.year}) - {movie.rating}
+            </li>
+          ))}
+        </ul>
+      </section>
       <section>
         <form onSubmit={handleSubmit}>
           <label htmlFor="movieTitle">Tittel</label>
